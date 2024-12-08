69be9961da2c83f577f0e7d6dd4aa90978ce2f31
Jeremiah Baccam <140921960+jbaccam@users.noreply.github.com>
2024-12-08 14:12:22 -0600

    Update file 12/8/2024, 2:12:22 PM

--- 206-ReverseLinkedList/206-ReverseLinkedList.java
+++ 206-ReverseLinkedList/206-ReverseLinkedList.java
@@ -1,15 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode prev = null;  
-        ListNode current = head;
-    
-        
-        while(current != null) { 
-            ListNode next = current.next; 
-            current.next = prev;
-            prev = current;
-            current = next;
+        ListNode prevNode = null; // initialize previous pointer to null
+        ListNode currentNode = head; // head will be the current position
+
+        while (currentNode != null) { // loop until we go through all the nodes
+            ListNode nextNode = currentNode.next; // create a next listNode and save it before we break the link
+
+            currentNode.next = prevNode; // reverse the pointer by setting the next to prev
+
+            // we can think of the two lines above as almost flipping the arrow
+            prevNode = currentNode; // now set prev forward to the currnet node
+
+            currentNode = nextNode; // increment and move the current forward to the next node
         }
-       return prev; 
+
+        return prevNode;
+
     }
 }
