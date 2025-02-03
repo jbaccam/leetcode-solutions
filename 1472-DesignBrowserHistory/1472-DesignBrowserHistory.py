37b00d85e0e4a6c9c720ce13384302a9908eb91a
Jeremiah Baccam <140921960+jbaccam@users.noreply.github.com>
2025-02-02 22:41:04 -0600

    Update file 2/2/2025, 10:41:04 PM

--- 1472-DesignBrowserHistory/1472-DesignBrowserHistory.py
+++ 1472-DesignBrowserHistory/1472-DesignBrowserHistory.py
@@ -1,30 +1,25 @@
-class ListNode:
-    def __init__(self, val, prev=None, next=None):
-        self.val = val
-        self.prev = prev
-        self.next = next
-
 class BrowserHistory:
 
     def __init__(self, homepage: str):
-        self.cur = ListNode(homepage)
+        self.i = 0 # current page pointer
+        self.len = 1 # current length
+        self.history = [homepage] 
 
     def visit(self, url: str) -> None:
-        self.cur.next = ListNode(url, self.cur)
-        self.cur = self.cur.next
-
+        if len(self.history) < self.i + 2:
+            self.history.append(url)
+        else: 
+            self.history[self.i + 1] = url
+        self.i += 1
+        self.len = self.i +1
 
     def back(self, steps: int) -> str:
-        while self.cur.prev and steps > 0:
-            self.cur = self.cur.prev
-            steps -= 1
-        return self.cur.val
+        self.i = max(self.i - steps, 0)
+        return self.history[self.i]
 
     def forward(self, steps: int) -> str:
-        while self.cur.next and steps > 0:
-            self.cur = self.cur.next
-            steps -= 1
-        return self.cur.val
+        self.i = min(self.i + steps, self.len -1)
+        return self.history[self.i]
         
 
 
