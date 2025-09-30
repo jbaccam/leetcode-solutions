e6f94877dc5da106504b0748f1869ac9c880c01d
Jeremiah Baccam <140921960+jbaccam@users.noreply.github.com>
2025-09-29 22:12:41 -0500

    Time: 9 ms (28.49%) | Memory: 24.2 MB (13.67%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -2,6 +2,11 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         count = {} # keeps count of the occurences
         bucket = [[] for i in range(len(nums)+1)] # our buckets, where each subarray is a bucket
+        # each bucket represents the count, so bucket at 3, would mean that the number in here has a count of 3
+        # so we know that the buckets on the right side, if they contain anything, are the most frequently
+        # counted numbers
+        # so we can find the frequency of all numbers in a hashmap, put them in the corresponding buckets
+        # and then we can append k amount of buckets at the end to our result and return that result
 
         for n in nums:
             count[n] = 1 + count.get(n, 0)
