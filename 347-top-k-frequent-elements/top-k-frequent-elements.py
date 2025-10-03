afa322dc0687281f78a0ffb3d2464dec527d2ce0
Jeremiah Baccam <140921960+jbaccam@users.noreply.github.com>
2025-10-03 18:17:41 -0500

    Time: 15 ms (10.03%) | Memory: 24.2 MB (20.43%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,24 +1,20 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {} # keeps count of the occurences
-        bucket = [[] for i in range(len(nums)+1)] # our buckets, where each subarray is a bucket
-        # each bucket represents the count, so bucket at 3, would mean that the number in here has a count of 3
-        # so we know that the buckets on the right side, if they contain anything, are the most frequently
-        # counted numbers
-        # so we can find the frequency of all numbers in a hashmap, put them in the corresponding buckets
-        # and then we can append k amount of buckets at the end to our result and return that result
+        count = {}
+        nums_bucket = [[] for i in range(len(nums)+1)]
+        res = []
 
         for n in nums:
             count[n] = 1 + count.get(n, 0)
-
-        for n, c in count.items():
-            bucket[c].append(n)
-         
-        res = []
-        for i in range(len(bucket)-1, 0, -1):
-            for j in bucket[i]:
+        
+        for n, i in count.items():
+            nums_bucket[i].append(n)
+        
+        for i in range(len(nums_bucket)-1, 0, -1):
+            for j in nums_bucket[i]:
                 res.append(j)
                 if len(res) == k:
                     return res
 
 
+        
