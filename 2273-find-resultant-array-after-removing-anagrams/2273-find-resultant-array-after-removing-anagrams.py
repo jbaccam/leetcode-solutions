afaea54f9062cc40c83ba624cb5e1a09384e4d42
Jeremiah Baccam <140921960+jbaccam@users.noreply.github.com>
2025-10-13 14:55:22 -0500

    Time: 25 ms (16.71%), Space: 18.1 MB (7.47%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
@@ -1,16 +1,24 @@
 class Solution:
     def removeAnagrams(self, words: List[str]) -> List[str]:
-        ans = []
-        prevWord = None
+        ans = [] # store the answers in here
+        prevWord = None # prevWord for comparision 
+
+        # iterate over every string
         for s in words:
+            # map currentWord to a counter which will allow us to do direct comparison
             currentWord = Counter(s)
+
+            # compare the current word, words[i] to words[i-1]
+            # if they arent anagrams, add words[i] to result
+            # if they are anagrams we dont add them at all
             if currentWord != prevWord:
                 ans.append(s)
                 prevWord = currentWord
         
         return ans
 
-
+# o(N * M)
+# num of words * lenth of words
         
                 
 
