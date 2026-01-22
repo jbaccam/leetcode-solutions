bfce38a530bf887d32b1b49ed213c2405ceb9d86
Jeremiah Baccam <140921960+jbaccam@users.noreply.github.com>
2026-01-21 19:18:10 -0600

    Time: 7 ms (90.78%) | Memory: 19.5 MB (12.89%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -2,12 +2,8 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        
-        countS = {}
-        countT = {}
 
-        for i in range(len(s)): # Right here we can just iterate over the length of either of strings since we know they're the same length
-            countS[s[i]] = 1 + countS.get(s[i], 0) # Use get instead of += 1 because if the value does not already exist in the dict then we would get an error
-            countT[t[i]] = 1 + countT.get(t[i], 0)
+        s2 = Counter(s)
+        t2 = Counter(t)
         
-        return countS == countT
+        return s2 == t2
